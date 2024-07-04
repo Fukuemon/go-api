a62225c82754ea46e4ff5168065d125031572fe2
Fukuemon <Fukuemony029@gmail.com>
2024-07-04 13:55:09 +0900

    ✨ feat: httpメソッドに応じて正しい処理を返すようにハンドラを修正

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,27 +11,52 @@ var responseMessage string = "Hello, World!"
 // 1. ハンドラの宣言
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	// ハンドラの処理
-	io.WriteString(w, responseMessage) // wにレスポンスを書き込む
+
+	if req.Method == http.MethodGet {
+		io.WriteString(w, responseMessage) // wにレスポンスを書き込む
+	} else {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article posted!")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Article posted!")
+	} else {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article list!")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article list!")
+	} else {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleId := 1
-	resString := fmt.Sprintf("Article detail! articleId: %d", articleId)
-	io.WriteString(w, resString)
+	if req.Method == http.MethodGet {
+		articleId := 1
+		resString := fmt.Sprintf("Article detail! articleId: %d", articleId)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Nice posted!")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Nice posted!")
+	} else {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Comment posted!")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Comment posted!")
+	} else {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
 }
