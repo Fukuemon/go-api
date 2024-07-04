22394e7c79f817cc501b7b16735ef0d7d09bec36
Fukuemon <Fukuemony029@gmail.com>
2024-07-04 13:03:05 +0900

    ✨ feat: net/http：サーバーの立ち上げ

--- main.go
+++ main.go
@@ -7,12 +7,25 @@ import (
 )
 
 func main() {
+	responseMessage := "Hello, World!"
+
+	// 1. ハンドラの宣言
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		// ハンドラの処理
+		io.WriteString(w, responseMessage) // wにレスポンスを書き込む
 	}
 
+	// ハンドラの登録
 	http.HandleFunc("/", helloHandler)
 
+	// サーバー起動時のログ出力
 	log.Println("Listening on port 8080")
+
+	//ListenAndServe関数：指定したポートでHTTPリクエストを待ち受ける
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	// 2行で書くパターン
+	// err := http.ListenAndServe(":8080", nil)
+	// log.Fatal(err)
 }
+
+// インターフェース型
